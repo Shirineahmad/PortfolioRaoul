172d78ef8fa34da2db15ea891949fa2566ffbaab
Shirineahmad <shirine.ahmad@hotmail.com>
2023-09-29 13:47:04 +0300

    design dashboard

--- src/pages/DashSkills.jsx
+++ src/pages/DashSkills.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const skills = () => {
   return (
     <div>
-      
+      <h1 style={{ textAlign: "center" }}>Skills</h1>
     </div>
-  )
+  );
 }
 
 export default skills
